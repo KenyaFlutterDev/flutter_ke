ae3b6ff96c790ff67568437c8fe148006dda312d
Michael Mboya <mikemboya2@gmail.com>
2023-08-15 17:43:36 +0300

    set up flutter dependencies

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp(),));
 }
 
 class MyApp extends StatelessWidget {
