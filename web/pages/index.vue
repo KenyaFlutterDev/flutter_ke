3e95abf556dc34dc7639d688f9b275bfb506086e
Michael Mboya <mikemboya2@gmail.com>
2023-08-22 01:20:25 +0300

    setup tailwind and change homepage

---
+++ web/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="w-screen h-screen bg-slate-100 dark:bg-slate-900 flex justify-center place-items-center">
+        <div class="flex flex-col text-slate-900 dark:text-slate-100 text-center">
+            <p class="text-2xl">Welcome To Flutter Kenya</p>
+            <p>We are currently developing the website. Your contributions would be welcome <NuxtLink to="https://github.com/KenyaFlutterDev/flutter_ke.git" class="cursor-pointer underline">Here</NuxtLink></p>
+        </div>
+    </div>
+</template>
