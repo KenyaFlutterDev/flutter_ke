51dce5007a67b90f0af5acd61e65376d2da487a0
Michael Mboya <mikemboya2@gmail.com>
2023-08-29 11:52:47 +0300

    add supabase provider

---
+++ mobile/lib/global/providers/supabase_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'supabase_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$supabaseHash() => r'2f5cd003e857ccac9737e69f02c3b0053cf9604f';
+
+/// See also [supabase].
+@ProviderFor(supabase)
+final supabaseProvider = AutoDisposeProvider<Supabase>.internal(
+  supabase,
+  name: r'supabaseProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$supabaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SupabaseRef = AutoDisposeProviderRef<Supabase>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
